9dd164cbff7b20e5380b1e5dcbc71213ff7841d9
Matheus Barbosa <matts.barbosa@gmail.com>
2023-07-05 19:38:29 -0300

    Parte 1 do projeto concluida

---
+++ src/ProductsList/index.tsx
@@ -0,0 +1,33 @@
+import Product from '../components/Product'
+import Game from '../models/Game'
+
+import { Container, List } from './styles'
+
+export type Props = {
+  title: string
+  background: 'grey' | 'black'
+  games: Game[]
+}
+
+const ProductsList = ({ title, background, games }: Props) => (
+  <Container background={background}>
+    <div className="container">
+      <h2>{title}</h2>
+      <List>
+        {games.map((game) => (
+          <Product
+            key={game.id}
+            category={game.category}
+            description={game.description}
+            image={game.image}
+            infos={game.infos}
+            system={game.system}
+            title={game.title}
+          />
+        ))}
+      </List>
+    </div>
+  </Container>
+)
+
+export default ProductsList
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { cores } from '../../styles'
+
+export const ButtonContainer = styled.button`
+  font-size: 16px;
+  font-weight: bold;
+  padding: 8px 16px;
+  background-color: transparent;
+  border: 2px solid ${cores.branca};
+  border-radius: 8px;
+  color: ${cores.branca};
+`
+
+export const ButtonLink = styled(Link)`
+  font-size: 16px;
+  font-weight: bold;
+  padding: 8px 16px;
+  text-decoration: none;
+  background-color: transparent;
+  border: 2px solid ${cores.branca};
+  border-radius: 8px;
+  color: ${cores.branca};
+`
